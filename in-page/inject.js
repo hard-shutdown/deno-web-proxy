adb4cd79f730fc32e24f3482b59a08dd35c1dd89
Cody <59001842+hard-shutdown@users.noreply.github.com>
2023-04-18 02:32:18 +0000

    csc

--- in-page/inject.js
+++ in-page/inject.js
@@ -3,8 +3,8 @@
 
     const proxiedHost = decodeURIComponent(new URL(window.location.href).searchParams.get('url'));
 
-    if(serviceWorker in navigator) {
-        navigator.serviceWorker.register('sw.js').then(registration => {
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('__sw.js').then(registration => {
             console.log('Service Worker registered: ', registration);
         }).catch(error => {
             console.log('Service Worker registration failed: ', error);
